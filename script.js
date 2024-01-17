767cc84fb3cca2763abbaf11d51ba99f1a4f08f4
andy256789 <andonovatanq3@gmail.com>
2024-01-17 16:59:24 +0400

    Added erase option

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const DEFAULT_SIZE = 16;
 const DEFAULT_MODE = "color";
 const DEFAULT_COLOR = "aliceblue";
+const BACKGROUND_COLOR = "#434343";
 let lastColor = DEFAULT_COLOR;
 let lastSize = DEFAULT_SIZE;
 let lastMode = DEFAULT_MODE;
@@ -10,6 +11,7 @@ const changeSize = document.querySelector(".size");
 const resetSketch = document.querySelector(".reset");
 
 const rainbow = document.querySelector(".rainbow");
+const erase = document.querySelector(".erase");
 const color = document.querySelector(".color");
 const darken = document.querySelector(".darken");
 
@@ -61,6 +63,14 @@ function changeColor(event,mode){
             if(event.type=="mousedown"){event.target.style.backgroundColor = lastColor;}
             if(mouseClicked===true){event.target.style.backgroundColor = lastColor;}
             break;
+        case "erase":
+            if(event.type=="mousedown"){event.target.style.backgroundColor = BACKGROUND_COLOR;
+                event.target.style.filter = `brightness(100%)`;
+                event.target.dataset.brightness = 100;}
+            if(mouseClicked===true){event.target.style.backgroundColor = BACKGROUND_COLOR; 
+                event.target.style.filter = `brightness(100%)`;
+                event.target.dataset.brightness = 100;}
+            break;
         case "rainbow":
             if(event.type=="mousedown"){event.target.style.backgroundColor = getRandomColor();}
             if(mouseClicked===true){event.target.style.backgroundColor = getRandomColor();}
@@ -68,6 +78,7 @@ function changeColor(event,mode){
         case "darken":
             if(event.type=="mousedown"){darkenBox(event);}
             if(mouseClicked===true){darkenBox(event);}
+            break;
 
     }
 }
@@ -93,6 +104,11 @@ rainbow.addEventListener("click", () => {
 color.addEventListener("click", () => {
     lastMode = "color";
 });
+
+erase.addEventListener("click",()=>{
+    lastMode = "erase";
+});
+
 darken.addEventListener("click", () => {
     lastMode = "darken";
 });
